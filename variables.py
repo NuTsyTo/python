d9033af6d04f8c96d5178d361768365af2b68de9
NuTsyTo <maxence.degaugue@gmail.com>
2023-06-07 16:56:48 +0200

    Premier commit

---
+++ variables.py
@@ -0,0 +1,47 @@
+# numéro entier,
+number1 = 11
+number1 = 14
+print(number1)
+
+# numéro à virgule variable
+number2 = 3.12
+number2 = 12.3
+print(number2)
+
+# chaine de caractères, string
+text1 = 'hello dude'
+print(text1)
+
+text2 = "goodbye"
+print(text2)
+
+text3 = """<div>
+        <h1>titre du niveau</h1>
+    </div>"""
+print(text3)
+
+#booléen, boolean
+vtt_is_good = True
+print(vtt_is_good)
+
+vtt_is_bad = False
+print(vtt_is_bad)
+
+#valeur nulle, nul value
+user_accepted_terms = None
+print(user_accepted_terms)
+foo = "None"
+
+print(type(user_accepted_terms))
+print(type(foo))
+print(type(number1))
+print(type(number2))
+print(type(text1))
+print(type(text2))
+print(type(text3))
+print(type(vtt_is_bad))
+
+print(type(number1) is int)
+print(type(text1) is bool)
+print(str(number2))
+
