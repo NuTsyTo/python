ea1d32a99bdffad5fb6a140aeb577fda27b9bb68
NuTsyTo <maxence.degaugue@gmail.com>
2023-06-14 12:07:31 +0200

    Ajout exercices 6

---
+++ exercices-06/exercice-06-06.py
@@ -0,0 +1,9 @@
+# exo 6.6
+# Affichez la taille de la liste
+
+my_list = ['foo', 'bar', 'baz', 'lorem', 'ipsum']
+
+# réponse 6.6
+
+my_list = ['foo', 'bar', 'baz', 'lorem', 'ipsum']
+print (len(my_list))
